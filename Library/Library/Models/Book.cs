208abce8cbab21160f4f8e33e0c1076240256f71
Mopano <p.r.7.norouzi@gmail.com>
2026-07-07 18:03:07 +0300

    Implementation Of Library

---
+++ Library/Library/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Models
+{
+    public class Book
+    {
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public int ISBN { get; set; }
+        public bool IsAvailable { get; set; } = true;
+    }
+}
---
+++ Library/Library/Models/Library.cs
@@ -0,0 +1,60 @@
+﻿namespace Library.Models
+{
+    public class Library
+    {
+        public List<Book> Books { get; set; } = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            try
+            {
+                Books.Add(book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error adding book: {ex.Message}", ex);
+            }
+        }
+
+        public void BorrowBook(string title)
+        {
+            try
+            {
+                var book = Books.FirstOrDefault(b => b.Title == title && b.IsAvailable);
+                if (book != null)
+                {
+                    book.IsAvailable = false;
+                }
+                else
+                {
+                    throw new Exception("Book not available");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{ex.Message}", ex);
+            }
+
+        }
+
+        public void ReturnBook(string title)
+        {
+            try
+            {
+                var book = Books.FirstOrDefault(b => b.Title == title && !b.IsAvailable);
+                if (book != null)
+                {
+                    book.IsAvailable = true;
+                }
+                else
+                {
+                    throw new Exception("Book not found or already available");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{ex.Message}", ex);
+            }
+        }
+    }
+}
